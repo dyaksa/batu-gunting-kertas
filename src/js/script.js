97eb9c9701f9ddcbabe352b7a95995f6418c09d5
dyaksa <diasnour0395@gmail.com>
2019-09-21 20:34:03 +0700

    refactor script javascript agar lebih simple

--- src/js/script.js
+++ src/js/script.js
@@ -1,58 +1,48 @@
 const btn1 = document.querySelectorAll('.row button');
-let scoreP = 0;
-let scoreComp = 0;
-let hasil = '';
 
-btn1[0].addEventListener('click', function (e) {
-    let i = angkaRandom(2, 0);
-    let comp = ['batu', 'gunting', 'kertas'];
-    let p = btn1[0].getAttribute('value');
-    if (p === comp[i]) {
-        hasil = 'Seri';
-    } else if (comp[i] === 'gunting') {
-        hasil = 'Maaf anda Kalah';
-        scoreComp++;
-    } else {
-        hasil = 'Selamat anda menang';
-        scoreP++;
-    }
-    let score = document.getElementById('score').innerHTML = scoreP + ' - ' + scoreComp;
-    document.getElementById('hasil').innerHTML = hasil;
+btn1.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        let comp = pilihanComp();
+        let p = btn.getAttribute('value');
+        let pesan = document.querySelector('div.hasil');
+        pesan.classList.replace('display-none', 'display-inline-block');
+        document.querySelector('.hasil p').innerHTML = hasil(comp, p);
+    });
 });
 
-btn1[1].addEventListener('click', function (e) {
-    let i = angkaRandom(2, 0);
+function pilihanComp() {
     let comp = ['batu', 'gunting', 'kertas'];
-    let p = btn1[1].getAttribute('value');
-    if (p === comp[i]) {
-        hasil = 'seri'
-    } else if (comp[i] === 'batu') {
-        hasil = 'Maaf anda kalah'
-        scoreComp++;
-    } else {
-        hasil = 'selamat anda menang'
-        scoreP++;
-    }
-    let score = document.getElementById('score').innerHTML = scoreP + ' - ' + scoreComp;
-    document.getElementById('hasil').innerHTML = hasil;
-});
+    let i = angkaRandom(2, 0);
+    return comp[i];
+}
 
-btn1[2].addEventListener('click', function (e) {
-    let i = angkaRandom(2, 0);
-    let comp = ['batu', 'gunting', 'kertas'];
-    let p = btn1[2].getAttribute('value');
-    if (p === comp[i]) {
-        hasil = 'seri';
-    } else if (comp[i] === 'gunting') {
-        hasil = 'maaf anda kalah';
-        scoreComp++;
+function hasil(comp, p) {
+    if (p === 'batu') {
+        if (p === comp) {
+            return 'seri';
+        } else if (comp === 'gunting') {
+            return "menang";
+        } else {
+            return "kalah";
+        }
+    } else if (p === 'gunting') {
+        if (p === comp) {
+            return 'seri';
+        } else if (comp === 'kertas') {
+            return 'menang';
+        } else {
+            return 'kalah';
+        }
     } else {
-        hasil = 'selamat anda menang';
-        scoreP++;
+        if (p === comp) {
+            return 'seri';
+        } else if (comp === 'batu') {
+            return 'menang';
+        } else {
+            return 'kalah';
+        }
     }
-    let score = document.getElementById('score').innerHTML = scoreP + ' - ' + scoreComp;
-    document.getElementById('hasil').innerHTML = hasil;
-});
+}
 
 function angkaRandom(high, low) {
     return Math.floor(Math.random() * (high - low + 1) + low);
